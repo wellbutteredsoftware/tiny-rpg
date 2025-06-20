44e841fda04b600e938185b38b270ded5585c6d7
wellbutteredtoast <swatsonjones@mun.ca>
2025-06-20 11:23:12 -0230

    hostile_npc stub

--- include/hostile_npc.h
+++ include/hostile_npc.h
@@ -1,2 +1,17 @@
 #include <raylib.h>
 #include <stdbool.h>
+
+#define BASE_REWARD_GOLD 5
+
+typedef struct HostileNPC Hostile NPC
+
+struct HostileNPC {
+    float x, y;
+    float vx, vy;
+    int current_health;
+    int max_health;
+    int heal_count;     /* hostiles can randomly heal some damage */
+    bool alive;
+    bool special;       /* reserved for bosses and the like   */
+    int reward;         /* pseudo-random reward for each kill */
+};
